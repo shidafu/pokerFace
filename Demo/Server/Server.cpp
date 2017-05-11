1efafa90b6896a3194d1dcf899d4a81d7da3c205
DESKTOP-4OF2T80\RWP <ping28198@126.com>
2017-05-11 19:00:13 +0800

    add libuv

--- Demo/Server/Server.cpp
+++ Demo/Server/Server.cpp
@@ -1,11 +1,25 @@
 // Server.cpp : 定义控制台应用程序的入口点。
 //
 
+
 #include "stdafx.h"
-
-
+#include "uWS.h"
+#include "cv.h"
 int main()
 {
+	uWS::Hub h;
+	uWS::Group<uWS::SERVER> *group = h.createGroup<uWS::SERVER>(uWS::PERMESSAGE_DEFLATE);
+
+	group->onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+
+		ws->send(message, length, opCode);
+
+	});
+	cv::Mat m;
+	h.listen(3000);
+	h.run();
+	
+
     return 0;
 }
 
