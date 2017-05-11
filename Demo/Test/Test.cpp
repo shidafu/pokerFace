aeddd2c2e44101d7c9d71138c33c6dceeb13b37b
towanglei <towanglei@163.com>
2017-05-11 14:16:51 +0800

    Fix include path.

--- Demo/Test/Test.cpp
+++ Demo/Test/Test.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
-
+#include<iostream>
+#include <opencv/cv.hpp>
+#include "face_identification.h"
+#include "common.h"
+using namespace std;
 
 int main()
 {
+
     return 0;
 }
 
